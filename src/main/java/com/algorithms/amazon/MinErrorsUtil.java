b29d15fe92e62b2bf78aa271ff41d349cf0dc7dd
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2024-10-23 13:22:03 +0300

    use arrays

--- src/main/java/com/algorithms/amazon/MinErrorsUtil.java
+++ src/main/java/com/algorithms/amazon/MinErrorsUtil.java
@@ -2,8 +2,6 @@ package com.algorithms.amazon;
 
 import org.apache.commons.math3.util.Pair;
 
-import java.util.HashMap;
-
 public class MinErrorsUtil {
     /**
      * a string "0!1!01"
@@ -15,8 +13,8 @@ public class MinErrorsUtil {
     public static Pair<String, Integer> getMinErrors(String errorString, int x, int y) {
         var chars = errorString.toCharArray();
 
-        var map0 = new HashMap<Integer, Integer>();
-        var map1 = new HashMap<Integer, Integer>();
+        var arr0 = new int[chars.length];
+        var arr1 = new int[chars.length];
         var count0 = 0;
         var count1 = 0;
         var errorsCount = 0;
@@ -27,8 +25,8 @@ public class MinErrorsUtil {
             } else if (chars[i] == '1') {
                 count1++;
             }
-            map0.put(i, count0);
-            map1.put(i, count1);
+            arr0[i] = count0;
+            arr1[i] = count1;
         }
 
         var added1Count = 0;
@@ -37,28 +35,28 @@ public class MinErrorsUtil {
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == '0') {
                 // 01 = x
-                var totalCountOf1 = map1.get(chars.length - 1);
-                var countOf1SoFar = map1.get(i);
+                var totalCountOf1 = arr1[chars.length - 1];
+                var countOf1SoFar = arr1[i];
                 var countOf1AfterCurrentChar = totalCountOf1 - countOf1SoFar;
                 errorsCount += countOf1AfterCurrentChar * x;
             } else if (chars[i] == '1') {
                 // 10 = y
-                var totalCountOf0 = map0.get(chars.length - 1);
-                var countOf0SoFar = map0.get(i);
+                var totalCountOf0 = arr0[chars.length - 1];
+                var countOf0SoFar = arr0[i];
                 var countOf0AfterCurrentChar = totalCountOf0 - countOf0SoFar;
                 errorsCount += countOf0AfterCurrentChar * y;
             } else if (chars[i] == '!') {
                 // what if we insert '0'
-                var countOf1BeforeCurrentChar = map1.get(i) + added1Count;
-                var totalCountOf1 = map1.get(chars.length - 1);
-                var countOf1SoFar = map1.get(i);
+                var countOf1BeforeCurrentChar = arr1[i] + added1Count;
+                var totalCountOf1 = arr1[chars.length - 1];
+                var countOf1SoFar = arr1[i];
                 var countOf1AfterCurrentChar = totalCountOf1 - countOf1SoFar;
                 var errorsIntroducedBy0 = countOf1BeforeCurrentChar * y // 1..0
                                           + countOf1AfterCurrentChar * x; // 0..1
                 // what if we insert '1'
-                var countOf0BeforeCurrentChar = map0.get(i) + added0Count;
-                var totalCountOf0 = map0.get(chars.length - 1);
-                var countOf0SoFar = map0.get(i);
+                var countOf0BeforeCurrentChar = arr0[i] + added0Count;
+                var totalCountOf0 = arr0[chars.length - 1];
+                var countOf0SoFar = arr0[i];
                 var countOf0AfterCurrentChar = totalCountOf0 - countOf0SoFar;
                 var errorsIntroducedBy1 = countOf0BeforeCurrentChar * x // 0..1
                                           + countOf0AfterCurrentChar * y; // 1..0
